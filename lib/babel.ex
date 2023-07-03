d80a3908a869b14c24eb51a7a8ea2164560ebee4
Sascha Wolf <swolf.dev@gmail.com>
2023-07-03 11:29:02 +0200

    :construction: babel: Add a placeholder top-level module

---
+++ lib/babel.ex
@@ -0,0 +1,33 @@
+defmodule Babel do
+  @type data :: Babel.Fetchable.t()
+
+  # @spec apply(t, data) :: {:ok, output} | {:error, Babel.Error.t()} when output: any
+  def apply!(_babel, _data) do
+    # babel.last_step
+    # |> Enum.reverse()
+    # |> Enum.reduce_while({:ok, data}, fn step, {:ok, data} ->
+    #   case Babel.Step.apply_one(step, data) do
+    #     {:ok, value} ->
+    #       {:cont, {:ok, value}}
+
+    #     {:error, error} ->
+    #       {:halt, {:error, error}}
+    #   end
+    # end)
+  end
+
+  # @spec apply(t(), data) :: {:ok, any}
+  # def apply(%Babel{} = babel, data) do
+  #   babel.reversed_steps
+  #   |> Enum.reverse()
+  #   |> Enum.reduce_while({:ok, data}, fn step, {:ok, next} ->
+  #     case Babel.Step.apply(step, next) do
+  #       {:ok, value} ->
+  #         {:cont, {:ok, value}}
+
+  #       {:error, details} ->
+  #         {:halt, {:error, details}}
+  #     end
+  #   end)
+  # end
+end
