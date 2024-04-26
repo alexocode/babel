38230dd7651af2bc8d4c672aee02b82d87e65391
Alex Wolf <craft@alexocode.dev>
2024-04-27 01:03:33 +0200

    :bulb: protocols/applicable: Add a TODO comment for future me

--- lib/babel/protocols/applicable.ex
+++ lib/babel/protocols/applicable.ex
@@ -3,6 +3,7 @@ defprotocol Babel.Applicable do
   @type t(output) :: t(any, output)
   @type t(_input, _output) :: any
 
+  # TODO: Add a <context> argument which should also be returned
   @spec apply(t(input, output), Babel.data()) :: {:ok, output} | {:error, Babel.Error.t()}
         when input: Babel.data(), output: any
   def apply(t, data)
