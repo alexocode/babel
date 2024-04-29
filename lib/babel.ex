3e5080405fe36aeb1495ad1a445ce003a831b314
Alex Wolf <craft@alexocode.dev>
2024-04-29 19:55:43 +0200

    :recycle: babel: Add non-pipeline versions for all functions

--- lib/babel.ex
+++ lib/babel.ex
@@ -1,8 +1,7 @@
 defmodule Babel do
-  import Kernel, except: [apply: 2]
+  import Kernel, except: [apply: 2, then: 2]
 
   alias Babel.Applicable
-  alias Babel.Core
   alias Babel.Error
   alias Babel.Pipeline
   alias Babel.Step
@@ -22,6 +21,8 @@ defmodule Babel do
 
   @typedoc "TODO: Better docs"
   @type path :: Core.path()
+
+  defguard is_babel(babel) when is_struct(babel, Babel.Pipeline) or is_struct(babel, Babel.Step)
 
   defmacro pipeline(name, [{:do, do_block} | maybe_else]) do
     case maybe_else do
@@ -48,72 +49,123 @@ defmodule Babel do
   def noop, do: id()
 
   @spec id() :: Step.t(input, input) when input: any
-  defdelegate id, to: Core
+  defdelegate id, to: Babel.Core
 
   @spec const(value) :: Step.t(value) when value: any
-  defdelegate const(value), to: Core
+  defdelegate const(value), to: Babel.Core
 
   @doc "Alias for `fetch/2`."
-  @spec at(t | nil, path) :: t
-  def at(babel \\ nil, path), do: fetch(babel, path)
+  @spec at(path) :: t
+  def at(path), do: fetch(path)
 
-  @spec fetch(t | nil, path) :: t
-  def fetch(babel \\ nil, path) do
-    chain(babel, Core.fetch(path))
+  @doc "Alias for `fetch/2`."
+  @spec at(t, path) :: t
+  def at(babel, path) do
+    fetch(babel, path)
   end
 
-  @spec get(t | nil, path) :: t
-  def get(babel \\ nil, path, default) do
-    chain(babel, Core.get(path, default))
+  @spec fetch(path) :: t
+  defdelegate fetch(path), to: Babel.Core
+
+  @spec fetch(t(), path) :: t
+  def fetch(babel, path) do
+    chain(babel, fetch(path))
   end
 
-  @spec cast(t | nil, :boolean) :: t(boolean)
-  @spec cast(t | nil, :integer) :: t(integer)
-  @spec cast(t | nil, :float) :: t(float)
-  def cast(babel \\ nil, target) do
-    chain(babel, Core.cast(target))
+  @spec get(path, default :: any) :: t
+  defdelegate get(path, default), to: Babel.Core
+
+  @spec get(t(), path, default :: any) :: t
+  def get(babel, path, default) do
+    chain(babel, get(path, default))
   end
 
-  @spec into(t | nil, intoable) :: t(intoable) when intoable: Babel.Intoable.t()
-  def into(babel \\ nil, intoable) do
-    chain(babel, Core.into(intoable))
+  @spec cast(:boolean) :: t(boolean)
+  @spec cast(:integer) :: t(integer)
+  @spec cast(:float) :: t(float)
+  defdelegate cast(type), to: Babel.Core
+
+  @spec cast(t(), :boolean) :: t(boolean)
+  @spec cast(t(), :integer) :: t(integer)
+  @spec cast(t(), :float) :: t(float)
+  def cast(babel, target) do
+    chain(babel, cast(target))
   end
 
-  @spec then(t(input) | nil, name, Step.fun(input, output)) :: t(output)
+  @spec into(intoable) :: t(intoable) when intoable: Babel.Intoable.t()
+  defdelegate into(intoable), to: Babel.Core
+
+  @spec into(t(), intoable) :: t(intoable) when intoable: Babel.Intoable.t()
+  def into(babel, intoable) do
+    chain(babel, into(intoable))
+  end
+
+  @spec then(Step.fun(input, output)) :: t(output) when input: any, output: any
+  def then(function), do: then(nil, function)
+
+  @spec then(t(input), Step.fun(input, output)) :: t(output)
         when input: data, output: term
-  def then(babel \\ nil, name \\ nil, function) do
-    chain(babel, Core.then(name, function))
+  def then(babel, function) when is_babel(babel) do
+    chain(babel, then(function))
   end
 
-  @spec call(t | nil, module, function_name :: atom) :: t
-  @spec call(t | nil, module, function_name :: atom, extra_args :: list) :: t
-  def call(babel \\ nil, module, function_name, extra_args \\ []) do
-    chain(babel, Core.call(module, function_name, extra_args))
-  end
+  @spec then(name, Step.fun(input, output)) :: t(output) when input: any, output: any
+  defdelegate then(descriptive_name, function), to: Babel.Core
 
-  @spec choice(t(input) | nil, (input -> t(input, output))) :: t(output)
+  @spec then(t(input), name, Step.fun(input, output)) :: t(output)
         when input: data, output: term
-  def choice(babel \\ nil, chooser) do
-    chain(babel, Core.choice(chooser))
+  def then(babel, descriptive_name, function) when is_babel(babel) do
+    chain(babel, then(descriptive_name, function))
   end
 
-  @spec map(t(Enumerable.t(input)) | nil, t(input, output)) :: t([output])
-        when input: data, output: term
-  def map(babel \\ nil, mapper) do
-    chain(babel, Core.map(mapper))
+  @spec call(module, function_name :: atom) :: t
+  def call(module, function_name), do: call(module, function_name, [])
+
+  @spec call(t, module, function_name :: atom) :: t
+  def call(babel, module, function_name) when is_babel(babel) do
+    chain(babel, call(module, function_name))
   end
 
-  @spec flat_map(t(Enumerable.t(input)) | nil, (input -> t(input, output))) :: t([output])
+  @spec call(module, function_name :: atom, extra_args :: list) :: t
+  defdelegate call(module, function_name, extra_args), to: Babel.Core
+
+  @spec call(t, module, function_name :: atom, extra_args :: list) :: t
+  def call(babel, module, function_name, extra_args) do
+    chain(babel, call(module, function_name, extra_args))
+  end
+
+  @spec choice((input -> t(input, output))) :: t(output) when input: data, output: term
+  defdelegate choice(chooser), to: Babel.Core
+
+  @spec choice(t(), (input -> t(input, output))) :: t(output)
         when input: data, output: term
-  def flat_map(babel \\ nil, mapper) do
-    chain(babel, Core.flat_map(mapper))
+  def choice(babel, chooser) do
+    chain(babel, choice(chooser))
+  end
+
+  @spec map(t(input, output)) :: t([output]) when input: data, output: term
+  defdelegate map(mapper), to: Babel.Core
+
+  @spec map(Pipeline.t(Enumerable.t(input)), t(input, output)) :: t([output])
+        when input: data, output: term
+  def map(babel, mapper) do
+    chain(babel, map(mapper))
+  end
+
+  @spec flat_map((input -> t(input, output))) :: t([output]) when input: data, output: term
+  defdelegate flat_map(mapper), to: Babel.Core
+
+  @spec flat_map(Pipeline.t(Enumerable.t(input)), (input -> t(input, output))) :: t([output])
+        when input: data, output: term
+  def flat_map(babel, mapper) do
+    chain(babel, flat_map(mapper))
   end
 
   @spec chain(nil, next) :: next when next: t
-  @spec chain(t(input, in_between), next :: t(in_between, output)) :: Pipeline.t(input, output)
-        when input: any, in_between: any, output: any
   def chain(nil, next), do: next
 
+  @spec chain(t(input, in_between), next :: t(in_between, output)) :: Pipeline.t(input, output)
+        when input: any, in_between: any, output: any
   def chain(babel, next) do
     babel
     |> Pipeline.new()
