30ccf7fb2299ff070b7c8483bc33562f244e1322
Alex Wolf <craft@alexocode.dev>
2024-05-10 00:41:35 +0200

    :white_check_mark: builtin/call: Test inspect/2

--- test/babel/builtin/call_test.exs
+++ test/babel/builtin/call_test.exs
@@ -39,4 +39,18 @@ defmodule Babel.Builtin.CallTest do
       assert {:error, %KeyError{key: :something}} = apply(step, %{})
     end
   end
+
+  describe "inspect/2" do
+    test "renders the step as expected" do
+      step_and_inspect = [
+        {Call.new(Map, :fetch, [:something]), "Babel.call(Map, :fetch, [:something])"},
+        {Call.new(NaiveDateTime, :from_iso8601), "Babel.call(NaiveDateTime, :from_iso8601)"},
+        {Call.new(NaiveDateTime, :from_iso8601, []), "Babel.call(NaiveDateTime, :from_iso8601)"}
+      ]
+
+      for {step, expected} <- step_and_inspect do
+        assert inspect(step) == expected
+      end
+    end
+  end
 end
