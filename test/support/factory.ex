abd7bf7b5de43d103393b72d598da5baa68a8e8f
Alex Wolf <craft@alexocode.dev>
2024-05-11 13:26:53 +0200

    :technologist: test - factory: Add a trace factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -35,4 +35,19 @@ defmodule Babel.Test.Factory do
   def step(value) do
     Babel.Builtin.Const.new(value)
   end
+
+  def trace(attrs \\ []) do
+    Babel.Trace.new(
+      Keyword.get_lazy(attrs, :babel, fn -> step() end),
+      Keyword.get_lazy(attrs, :input, fn -> data() end),
+      Keyword.get_lazy(attrs, :output, fn -> data() end),
+      Keyword.get_lazy(attrs, :nested, fn -> random_nr_of(fn -> trace(nested: []) end, 0..10) end)
+    )
+  end
+
+  defp random_nr_of(generator, min..max = range) when min <= max do
+    generator
+    |> Stream.repeatedly()
+    |> Enum.take(Enum.random(range))
+  end
 end
