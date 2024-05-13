8c89c4939971a6111a2941dfb780fa4202c17a31
Alex Wolf <craft@alexocode.dev>
2024-05-13 17:38:21 +0200

    :white_check_mark: trace/nesting: Add tests where necessary

--- lib/babel/trace/nesting.ex
+++ lib/babel/trace/nesting.ex
@@ -46,11 +46,29 @@ defmodule Babel.Trace.Nesting do
     {Enum.reverse(traces), accumulated}
   end
 
+  @doc "Combines `collect_oks/2` and `collect_errors/2`. When `collect_oks/2` returns `nil` it calls `collect_errors/2`."
   @spec collect_results(result(any), result([any])) :: result([any])
   def collect_results(result, collected) do
     collect_oks(result, collected) || collect_errors(result, collected)
   end
 
+  @doc """
+  Aggregates `:ok` results. Returns `nil` for `:error` results.
+
+  ## Examples
+
+      iex> collect_oks({:ok, :some_value}, {:ok, []})
+      {:ok, [:some_value]}
+
+      iex> collect_oks({:ok, :another_value}, {:ok, [:some_value]})
+      {:ok, [:another_value, :some_value]}
+
+      iex> collect_oks({:error, :some_reason}, {:ok, [:some_value, :another_value]})
+      nil
+
+      iex> collect_oks({:ok, :some_value}, {:error, [:some_reason]})
+      nil
+  """
   @spec collect_oks({:ok, value}, {:ok, [value]}) :: {:ok, [value]} when value: any
   def collect_oks(result, {:ok, l}) do
     case result do
@@ -62,6 +80,23 @@ defmodule Babel.Trace.Nesting do
   @spec collect_oks({:error, reason :: any}, {:ok, list}) :: nil
   def collect_oks(_result, {:error, _}), do: nil
 
+  @doc """
+  Aggregates `:error` results. Ignores `:ok` results.
+
+  ## Examples
+
+      iex> collect_errors({:error, :some_reason}, {:ok, [:some_value, :another_value]})
+      {:error, [:some_reason]}
+
+      iex> collect_errors({:error, :another_reason}, {:error, [:some_reason]})
+      {:error, [:another_reason, :some_reason]}
+
+      iex> collect_errors({:error, [:further_reason, :another_reason]}, {:error, [:some_reason]})
+      {:error, [:another_reason, :further_reason, :some_reason]}
+
+      iex> collect_errors({:ok, :another_value}, {:ok, [:some_value]})
+      nil
+  """
   @spec collect_errors({:ok, any}, {:ok, any}) :: nil
   @spec collect_errors({:error, reason}, {:ok, any}) :: {:error, [reason]} when reason: any
   def collect_errors(result, {:ok, _}) do
