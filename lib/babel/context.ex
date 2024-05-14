f677b6f98e20b059f9e61cac559bf396281238e0
Alex Wolf <craft@alexocode.dev>
2024-05-14 22:40:52 +0200

    :recycle: context: Remove `original` and replace it with `history`

--- lib/babel/context.ex
+++ lib/babel/context.ex
@@ -1,13 +1,20 @@
 defmodule Babel.Context do
+  @moduledoc """
+  Passed to `Babel.Applicable.apply/2` wrapping the given `data`.
+
+  In addition it retains a `history` of all previously executed steps by
+  capturing their `Babel.Trace`s.
+  """
   @type t :: t(any)
   @type t(data) :: %__MODULE__{
           data: data,
-          original: Babel.data()
+          history: [Babel.Trace.t()]
         }
-  defstruct [:data, :original]
+  defstruct [:data, {:history, []}]
 
   @spec new(data) :: t(data) when data: Babel.data()
-  def new(data) do
-    %__MODULE__{data: data, original: data}
+  @spec new(data, history :: [Babel.Trace.t()]) :: t(data) when data: Babel.data()
+  def new(data, history \\ []) do
+    %__MODULE__{data: data, history: history}
   end
 end
