1cd5ec16974b1deb902e3106b05ba18f33f85119
Alex Wolf <craft@alexocode.dev>
2024-05-14 22:18:38 +0200

    :technologist: test: Extract Babel.Test.StepCase helper functions into a contained module

---
+++ lib/babel/test.ex
@@ -0,0 +1,28 @@
+defmodule Babel.Test do
+  @moduledoc """
+  You can use this module to test your custom `Babel.Step`s.
+
+  It defines two functions:
+  - `apply/2`
+  - `apply!/2`
+
+  The difference to `Babel.apply/2` and `Babel.apply!/2` is that they return
+  the "raw" result from the step, including in an error case (nothing gets
+  wrapped in a `Babel.Error`).
+
+  `apply!/2` raises by `assert`ing on the `{:ok, value}` shape of the `apply/2` result.
+  """
+  import ExUnit.Assertions
+  import Kernel, except: [apply: 2]
+
+  def apply(step, data) do
+    step
+    |> Babel.trace(data)
+    |> Babel.Trace.result()
+  end
+
+  def apply!(step, data) do
+    assert {:ok, value} = apply(step, data)
+    value
+  end
+end
--- test/support/step_case.ex
+++ test/support/step_case.ex
@@ -1,25 +1,12 @@
 defmodule Babel.Test.StepCase do
+  @moduledoc false
   use ExUnit.CaseTemplate
-
-  import Kernel, except: [apply: 2]
 
   using do
     quote do
       import Kernel, except: [apply: 2]
-      import Babel.Test.StepCase
+      import Babel, only: [trace: 2]
+      import Babel.Test
     end
-  end
-
-  def trace(step, data), do: Babel.trace(step, data)
-
-  def apply(step, data) do
-    step
-    |> trace(data)
-    |> Babel.Trace.result()
-  end
-
-  def apply!(step, data) do
-    {:ok, value} = apply(step, data)
-    value
   end
 end
